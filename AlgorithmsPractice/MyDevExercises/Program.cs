0d67dc2f12798878bf9263ee7a95f84370c8d76e
HernandoNJ <hernandonj.unity@gmail.com>
2025-08-29 23:20:09 -0500

    added Roll A Dice, refactored MyDevExercises\Exercises

---
+++ AlgorithmsPractice/MyDevExercises/Program.cs
@@ -0,0 +1,8 @@
+﻿using AlgorithmsTest;
+
+var ex = new Exercises();
+//ex.PrintNewWords();
+//ex.PrintArraySumText();
+//ex.CheckIntArrayValues();
+//ex.TripletsCalculation();
+ex.CalculateVeryBigSum();
