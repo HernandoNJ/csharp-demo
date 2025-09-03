d2a338c30d62cf63ab69440240d011b1b06aca4b
HernandoNJ <hernandonj.unity@gmail.com>
2025-09-02 23:31:30 -0500

    first functional version done

--- AlgorithmsPractice/RollADice/Program.cs
+++ AlgorithmsPractice/RollADice/Program.cs
@@ -1,5 +1,14 @@
-﻿var dice = new Dice(new Random());
-Console.WriteLine(dice.Roll());
+﻿var triesCount = 0;
+
+var dice = new Dice(new Random());
+var diceNumber = dice.Roll();
+Console.WriteLine("The dice number is: " + diceNumber); // only for testing
+
+dice.HandleUserInput(diceNumber, ref triesCount);
+
+Console.WriteLine("tries count is: " + triesCount); // only for testing
+Console.WriteLine("Press a key to exit");
+
 Console.ReadKey();
 
 public class Dice
@@ -9,4 +18,31 @@ public class Dice
     public Dice(Random random) => _random = random;
 
     public int Roll() => _random.Next(1, 7);
+    
+    public void HandleUserInput(int i, ref int triesCountArg)
+    {
+        do
+        {
+            Console.WriteLine("Enter a number");
+            var newInput = Console.ReadLine();
+            var userInput = Convert.ToInt32(newInput);
+            triesCountArg++;
+
+            if (userInput == i)
+            {
+                Console.WriteLine("You win!");
+                return;
+            }
+        
+            if (triesCountArg < 3)
+            {
+                Console.WriteLine("Try again!");
+            }
+            else if (triesCountArg == 3)
+            {
+                Console.WriteLine("You lose!");
+                break;
+            }
+        } while (triesCountArg < 3);
+    }
 }
