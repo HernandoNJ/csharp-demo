fb89c0b798b84d982fd4b391d82e3e468212afa6
HernandoNJ <hernandonj.unity@gmail.com>
2025-09-11 11:41:42 -0500

    cleaned up Pizza class

--- AlgorithmsPractice/PizzaApp/Pizza.cs
+++ AlgorithmsPractice/PizzaApp/Pizza.cs
@@ -3,20 +3,6 @@ namespace PizzaApp;
 public class Pizza
 {
     private List<Ingredient> _ingredients = new List<Ingredient>();
-    public void AddIngredient(Ingredient ingredient)=> _ingredients.Add(ingredient);
-
-    // Removed Describe()
-    // Instead, using ToString to describe the pizza class
-    public override string ToString() 
-        => $"This is a pizza with: { string.Join(", ", _ingredients)}";
-    
-    // *******************************************************************
-    // public string Describe() 
-    //     => $"This is a pizza with: { string.Join(", ", 
-    //         _ingredients.Select(i => i.Name))}";
-    
-    // public string Describe() 
-    //     => $"This is a pizza with: { string.Join(", ", 
-    //         _ingredients)}";
-    // Result: This is a pizza with: PizzaApp.Mozarella, PizzaApp.Cheddar, PizzaApp.TomatoSouce
+    public void AddIngredient(Ingredient ingredient) => _ingredients.Add(ingredient);
+    public override string ToString() => $"This pizza contains {string.Join(", ", _ingredients)}";
 }
