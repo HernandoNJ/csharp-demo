0e7b83d254a1625d6275befe9ed7eef0c4086f28
HernandoNJ <hernandonj.unity@gmail.com>
2025-09-22 21:07:36 -0500

    project done, own version

---
+++ AlgorithmsPractice/UCS_Solution/CookiesCookBook/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace CookiesCookBook
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public Recipe(int id, List<Ingredient> ingredients)
+        {
+            ID = id;
+            Ingredients = ingredients;
+        }
+    }
+}
