8f7f9976d003da1cb60e82ad549bf8fa9f99f6d1
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-03 15:17:57 -0500

    code cleanup

---
+++ AlgorithmsPractice/UCS_Solution/CookiesCookBook/App/IRecipesUserInteraction.cs
@@ -0,0 +1,13 @@
+﻿using CookiesCookBook.Recipes;
+using CookiesCookBook.Recipes.Ingredients;
+
+namespace CookiesCookBook.App;
+
+public interface IRecipesUserInteraction
+{
+    void ShowMessage(string message);
+    void Exit();
+    void PrintExistingRecipes(IEnumerable<Recipe> allRecipes);
+    void PromptToCreateRecipe();
+    IEnumerable<Ingredient> ReadIngredientsFromUser();
+}
---
+++ AlgorithmsPractice/UCS_Solution/CookiesCookBook/App/RecipesConsoleUserInteraction.cs
@@ -0,0 +1,80 @@
+﻿using CookiesCookBook.Recipes;
+using CookiesCookBook.Recipes.Ingredients;
+
+namespace CookiesCookBook.App;
+
+public class RecipesConsoleUserInteraction : IRecipesUserInteraction
+{
+    private readonly IIngredientsRegister _ingredientsRegister;
+
+    public RecipesConsoleUserInteraction(IIngredientsRegister ingredientsRegister)
+    {
+        _ingredientsRegister = ingredientsRegister;
+    }
+
+    public void PromptToCreateRecipe()
+    {
+        Console.WriteLine("Create a new cookie recipe! Availabe ingredients are: ");
+        foreach (var ingredient in _ingredientsRegister.All)
+        {
+            Console.WriteLine(ingredient);
+        }
+    }
+
+    public void PrintExistingRecipes(IEnumerable<Recipe> allRecipes)
+    {
+        if (allRecipes.Count() > 0)
+        {
+            Console.WriteLine("Existing recipes: " + Environment.NewLine);
+            int counter = 1;
+            foreach (var recipe in allRecipes)
+            {
+                Console.WriteLine($"*** {counter} ***");
+                Console.WriteLine(recipe);
+                Console.WriteLine();
+                counter++;
+            }
+        }
+    }
+
+    public IEnumerable<Ingredient> ReadIngredientsFromUser()
+    {
+        bool shallStop = false;
+        var ingredients = new List<Ingredient>();
+
+        while (!shallStop)
+        {
+            Console.WriteLine("Add an ingredient by Id. Type any other key to finish.");
+
+            var userInput = Console.ReadLine();
+            var intInput = int.TryParse(userInput,out int id);
+
+            if (intInput)
+            {
+                var selectedIngredient = _ingredientsRegister.GetById(id);
+
+                if (selectedIngredient is not null)
+                {
+                    ingredients.Add(selectedIngredient);
+                }
+            }
+            else
+            {
+                shallStop = true;
+            }
+        }
+
+        return ingredients;
+    }
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
