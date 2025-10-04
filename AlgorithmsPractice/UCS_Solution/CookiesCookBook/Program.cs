78127dfc0c8051f7975374b42c2d628915e20c6a
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-04 15:46:38 -0500

    global try-catch

--- AlgorithmsPractice/UCS_Solution/CookiesCookBook/Program.cs
+++ AlgorithmsPractice/UCS_Solution/CookiesCookBook/Program.cs
@@ -4,21 +4,34 @@ using CookiesCookBook.FileAccess;
 using CookiesCookBook.Recipes;
 using CookiesCookBook.Recipes.Ingredients;
 
+// Global try-catch
+try
+{
+    const FileFormat Format = FileFormat.Txt;
+    const string FileName = "recipes";
+    var fileMetaData = new FileMetaData(FileName,Format);
+    var filePath = fileMetaData.ToPath();
 
-const FileFormat Format = FileFormat.Txt;
-const string FileName = "recipes";
-var fileMetaData = new FileMetaData(FileName,Format);
-var filePath = fileMetaData.ToPath();
+    IStringsRepository stringsRepository =
+        Format == FileFormat.Json ?
+        new StringsJsonRepository() :
+        new StringsTextRepository();
 
-IStringsRepository stringsRepository =
-    Format == FileFormat.Json ?
-    new StringsJsonRepository() :
-    new StringsTextRepository();
+    var ingredientsRegister = new IngredientsRegister();
 
-var ingredientsRegister = new IngredientsRegister();
+    var cookiesRecipesApp = new CookiesRecipesApp(
+        new RecipesRepository(stringsRepository,ingredientsRegister),
+        new RecipesConsoleUserInteraction(ingredientsRegister));
 
-var cookiesRecipesApp = new CookiesRecipesApp(
-    new RecipesRepository(stringsRepository,ingredientsRegister),
-    new RecipesConsoleUserInteraction(ingredientsRegister));
+    cookiesRecipesApp.Run(filePath);
+}
+catch (Exception ex)
+{
+    // Log implementation
 
-cookiesRecipesApp.Run(filePath);
+    // Message for the user
+    Console.WriteLine("Sorry! the app has an unexpected error and needs to be closed.");
+    Console.WriteLine("Error message: " + ex.Message);
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadKey();
+}
