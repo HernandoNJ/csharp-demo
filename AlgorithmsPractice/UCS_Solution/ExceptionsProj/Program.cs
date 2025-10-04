07d9691fa438cc1b8be6b32ac4861428d72cd508
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-03 22:55:05 -0500

    throw and throw ex

--- AlgorithmsPractice/UCS_Solution/ExceptionsProj/Program.cs
+++ AlgorithmsPractice/UCS_Solution/ExceptionsProj/Program.cs
@@ -1,14 +1,11 @@
-﻿var invalidPersonObject = new Person("",-100);
-var emptyCollection = new List<int>();
-var firstElement = GetFirstElement(emptyCollection);
-var firstUsingLinq = emptyCollection.First(); // InvalidOperationException
+﻿try
+{
+    var result = IsFirstElementPositive2(null);
+}
+catch (ArgumentNullException ex)
+{
 
-var numbers = new int[] { 1,2,3 };
-var fourth = numbers[3]; // OutOfIndexException
-
-bool has7 = CheckIfContains(7,numbers);
-
-RecursiveMethod2(0);
+}
 
 Console.ReadKey();
 
@@ -22,49 +19,59 @@ int GetFirstElement(IEnumerable<int> numbers)
     throw new InvalidOperationException("The collection cannot be empty.");
 }
 
-bool CheckIfContains(int value,int[] numbers)
+bool IsFirstElementPositive(IEnumerable<int> numbers)
 {
-    // Prefer not to use this exception
-    // Only in case we want to express the method should not be used
-    // because it is still in progress
-    throw new NotImplementedException();
-}
-
-void RecursiveMethod1()
-{
-    Console.WriteLine("Calling method1");
-
-    // Recursive call generates a StackOverflowException
-    RecursiveMethod1();
-}
-
-void RecursiveMethod2(int counter)
-{
-    Console.WriteLine("Calling method2. counter: " + counter);
-
-    // preventing a StackOverflowException by adding a counter
-    if (counter < 10)
+    // Required to design this method to return true
+    // if the collection is empty
+    try
     {
-        RecursiveMethod2(counter + 1);
+        var first = GetFirstElement(numbers);
+        return first > 0;
     }
+    catch (Exception ex)
+    {
+        // If the collection is null, this exception is called
+        // returning true
+        // because it is the exception's type base class
+        // requirement: empty => true, not null collection
+        // It may lead to confusion or false information
+        Console.WriteLine("Collection is empty");
+        return true;
+    }
+
 }
 
-class Person
+bool IsFirstElementPositive2(IEnumerable<int> numbers)
 {
-    public string Name { get; }
-    public int YearOfBirth { get; }
-
-    public Person(string name,int yearOfBirth = 0)
+    try
     {
-        if (name is null)
-            throw new ArgumentNullException("Name cannot be null");
-        if (name == string.Empty)
-            throw new ArgumentException("Name cannot be empty.");
+        var first = GetFirstElement(numbers);
+        return first > 0;
+    }
+    // Aligned with the GetFirstElement() exception
+    // caused by an empty collection
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine("Collection is empty");
+        return true;
+    }
+    // Another possible exception, but it only says there is a null collection
+    catch (NullReferenceException ex)
+    {
+        // More precise, this ANE ex contains the NRE ex
+        //throw new ArgumentNullException("Null collection. ",ex);
 
-        if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
-            throw new ArgumentOutOfRangeException("Year of birth between 1900 and current year");
+        // Re throwing
+        // Throwing the same object without additional processing
+        // Unnecessary, and instead the exception from
+        // GetFirstElement(numbers) would be called
+        // throw ex;
 
-        Name = name;
-        YearOfBirth = yearOfBirth;
+        // Rethrowing makes sense if we want to perform additional steps
+        // e.g, show a message to the user
+        // or write the info about the issue in the logs
+        //Console.WriteLine("Sorry, unexpected error.");
+        //throw;
+        throw new ArgumentNullException("Null collection. ",ex);
     }
 }
