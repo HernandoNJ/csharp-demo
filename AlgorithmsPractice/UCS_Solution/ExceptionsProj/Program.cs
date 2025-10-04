6922c71a8b64b7d113c6d33b56f74947dd9359cd
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-04 15:43:41 -0500

    generic exception

--- AlgorithmsPractice/UCS_Solution/ExceptionsProj/Program.cs
+++ AlgorithmsPractice/UCS_Solution/ExceptionsProj/Program.cs
@@ -1,77 +1,60 @@
-﻿try
-{
-    var result = IsFirstElementPositive2(null);
-}
-catch (ArgumentNullException ex)
-{
-
-}
-
+﻿Console.WriteLine("Hello");
 Console.ReadKey();
 
-int GetFirstElement(IEnumerable<int> numbers)
+public class PersonDataReader
 {
-    foreach (int number in numbers)
+    private readonly IPeopleRepository _peopleRepository;
+    private readonly ILogger _logger;
+
+    public PersonDataReader(
+        IPeopleRepository peopleRepository,
+        ILogger logger)
     {
-        return number;
+        _peopleRepository = peopleRepository;
+        _logger = logger;
     }
 
-    throw new InvalidOperationException("The collection cannot be empty.");
+    public Person ReadPersonData(int personId)
+    {
+        try
+        {
+            // What kind of exception throw? hard to know
+            // IPeopleRepository is implemented
+            // but the exact type is not known
+            // any exception may happen
+            // here a generic exception is ok
+            // because the exception is logged and then rethrown
+            return _peopleRepository.Read(personId);
+        }
+        catch (Exception ex)
+        {
+            _logger.Log(ex);
+            throw new Exception();
+        }
+    }
+
+
 }
 
-bool IsFirstElementPositive(IEnumerable<int> numbers)
+public interface ILogger
 {
-    // Required to design this method to return true
-    // if the collection is empty
-    try
-    {
-        var first = GetFirstElement(numbers);
-        return first > 0;
-    }
-    catch (Exception ex)
-    {
-        // If the collection is null, this exception is called
-        // returning true
-        // because it is the exception's type base class
-        // requirement: empty => true, not null collection
-        // It may lead to confusion or false information
-        Console.WriteLine("Collection is empty");
-        return true;
-    }
-
+    void Log(Exception ex);
 }
 
-bool IsFirstElementPositive2(IEnumerable<int> numbers)
+public interface IPeopleRepository
 {
-    try
-    {
-        var first = GetFirstElement(numbers);
-        return first > 0;
-    }
-    // Aligned with the GetFirstElement() exception
-    // caused by an empty collection
-    catch (InvalidOperationException ex)
-    {
-        Console.WriteLine("Collection is empty");
-        return true;
-    }
-    // Another possible exception, but it only says there is a null collection
-    catch (NullReferenceException ex)
-    {
-        // More precise, this ANE ex contains the NRE ex
-        //throw new ArgumentNullException("Null collection. ",ex);
+    Person Read(int id);
+}
 
-        // Re throwing
-        // Throwing the same object without additional processing
-        // Unnecessary, and instead the exception from
-        // GetFirstElement(numbers) would be called
-        // throw ex;
+public class Logger : ILogger
+{
+    public void Log(Exception ex)
+    {
 
-        // Rethrowing makes sense if we want to perform additional steps
-        // e.g, show a message to the user
-        // or write the info about the issue in the logs
-        //Console.WriteLine("Sorry, unexpected error.");
-        //throw;
-        throw new ArgumentNullException("Null collection. ",ex);
     }
+}
+
+public class Person
+{
+    public int Id { get; }
 }
