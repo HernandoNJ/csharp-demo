b4ef257ca9e338b3a9852a462726f2fbe6ffe085
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-05 15:47:34 -0500

    exceptions: video games list project

--- AlgorithmsPractice/UCS_Solution/GameDataParser/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GameDataParser/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+Console.WriteLine("Enter the file name.");
+var fileNameInput = Console.ReadLine();
+
+// Provide a Json string
+var fileContents = File.ReadAllText(fileNameInput);
+
+// Deserialize the Json string
+var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+
+if (videoGames.Count > 0)
+{
+    foreach (var videogame in videoGames)
+    {
+        Console.WriteLine(videogame);
+        Console.WriteLine();
+    }
+}
+else Console.WriteLine("No games found.");
+
+Console.ReadKey();
+
+public class VideoGame
+{
+    public string Title { get; init; }
+    public int ReleaseYear { get; init; }
+    public double Rating { get; init; }
+
+    public override string ToString()
+    {
+        return $"{Title}, {ReleaseYear}, {Rating}";
+    }
+}
