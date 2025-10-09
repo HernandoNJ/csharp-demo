0a280e43674f8daf67ff6a80abaddfb7b30e755b
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-08 22:41:45 -0500

    built-in tuples

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,13 +1,17 @@
 ﻿var numbers = new List<int>() { 5,3,2,8,17,7 };
 
+var twoStrings = new Tuple<string,string>("aaa","bbb");
+var differentTypes = new Tuple<string,int>("aaa",1);
+var threeTypes = new Tuple<string,int,bool>("aaa",1,true);
+
 //SimpleTwoInts minAndMax = GetMinMax(numbers);
-SimpleTuple<int,int> minAndMax = GetMinMaxWithTuples(numbers);
+Tuple<int,int> minAndMax = GetMinMaxWithTuples(numbers);
 Console.WriteLine($"Min is {minAndMax.Item1}");
 Console.WriteLine($"Max is {minAndMax.Item2}");
 
 Console.ReadKey();
 
-SimpleTuple<int,int> GetMinMaxWithTuples(IEnumerable<int> inputList)
+Tuple<int,int> GetMinMaxWithTuples(IEnumerable<int> inputList)
 {
     if (!inputList.Any())
         throw new InvalidOperationException(
@@ -21,7 +25,8 @@ SimpleTuple<int,int> GetMinMaxWithTuples(IEnumerable<int> inputList)
         if (number > max) max = number;
         if (number < min) min = number;
     }
-    return new SimpleTuple<int,int>(min,max);
+
+    return new Tuple<int,int>(min,max);
 }
 
 /*Creating a new type to store 2 ints
@@ -34,43 +39,57 @@ SimpleTuple<int,int> GetMinMaxWithTuples(IEnumerable<int> inputList)
  3. Iterate the collection
  4. Compare each item with the variables we have
  5. Return the values */
-SimpleTwoInts GetMinMax(IEnumerable<int> inputList)
-{
-    if (!inputList.Any())
-        throw new InvalidOperationException(
-            $"The input collection cannot be empty.");
+//SimpleTwoInts GetMinMax(IEnumerable<int> inputList)
+//{
+//    if (!inputList.Any())
+//        throw new InvalidOperationException(
+//            $"The input collection cannot be empty.");
 
-    int min = inputList.First();
-    int max = inputList.First();
+//    int min = inputList.First();
+//    int max = inputList.First();
 
-    foreach (int number in numbers)
-    {
-        if (number > max) max = number;
-        if (number < min) min = number;
-    }
-    return new SimpleTwoInts(min,max);
-}
+//    foreach (int number in numbers)
+//    {
+//        if (number > max) max = number;
+//        if (number < min) min = number;
+//    }
+//    return new SimpleTwoInts(min,max);
+//}
 
-public class SimpleTuple<T1, T2>
-{
-    public T1 Item1 { get; }
-    public T2 Item2 { get; }
+//public class SimpleTuple<T1, T2>
+//{
+//    public T1 Item1 { get; }
+//    public T2 Item2 { get; }
 
-    public SimpleTuple(T1 item1,T2 item2)
-    {
-        Item1 = item1;
-        Item2 = item2;
-    }
-}
+//    public SimpleTuple(T1 item1,T2 item2)
+//    {
+//        Item1 = item1;
+//        Item2 = item2;
+//    }
+//}
 
-public class SimpleTwoInts
-{
-    public int Int1 { get; }
-    public int Int2 { get; }
+//public class SimpleTuple<T1, T2, T3>
+//{
+//    public T1 Item1 { get; }
+//    public T2 Item2 { get; }
+//    public T3 Item3 { get; }
 
-    public SimpleTwoInts(int int1,int int2)
-    {
-        Int1 = int1;
-        Int2 = int2;
-    }
-}
+//    public SimpleTuple(T1 item1,T2 item2, T3 item3)
+//    {
+//        Item1 = item1;
+//        Item2 = item2;
+//        Item3 = item3;
+//    }
+//}
+
+//public class SimpleTwoInts
+//{
+//    public int Int1 { get; }
+//    public int Int2 { get; }
+
+//    public SimpleTwoInts(int int1,int int2)
+//    {
+//        Int1 = int1;
+//        Int2 = int2;
+//    }
+//}
