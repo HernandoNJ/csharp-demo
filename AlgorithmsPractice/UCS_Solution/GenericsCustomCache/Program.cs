677e83236c72548376b058867004cbacdb7b23a7
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-09 12:57:54 -0500

    generic method with constraints

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,57 +1,30 @@
-﻿var ints = new List<int>() { 1,2,3 };
-// The type of T is inferred because
-// ints is a List<int>, T is int
-// And so the item
-// ints.AddToFront<int>(12);
-ints.AddToFront(10);
+﻿// This constructor is not parameterless
+// var points = CreateCollectionOfRandomLengthWithConstraints<Point>(10); 
 
-var decimals = new List<decimal> { 1.1m,0.5m,22.5m,12m };
-var ints1 = decimals.ConverToIntList();
-var ints2 = decimals.ConvertTo<decimal,int>();
-
-var floats = new List<float> { 1.4f,100.01f };
-var longs = floats.ConvertTo<float,decimal>();
-
-var dates = new List<DateTime> { new DateTime(2023) };
-var ints3 = dates.ConvertTo<DateTime,int>(); // won't work
-
+// Parameterless constructor
+var ints = CreateCollectionOfRandomLengthWithConstraints<int>(10);
 
 Console.ReadKey();
 
-public static class ListExtensions
+// Allow only T types with parameterless constructor
+IEnumerable<T> CreateCollectionOfRandomLengthWithConstraints<T>(
+    int maxLength) where T : new()
 {
-    public static void AddToFront<T>(this List<T> list,T item)
+    var randomLength = new Random().Next(maxLength + 1);
+
+    var resultList = new List<T>();
+    for (int i = 0; i < randomLength; i++) resultList.Add(new T());
+
+    return resultList;
+}
+
+public class Point
+{
+    public int x, y;
+
+    public Point(int x,int y)
     {
-        list.Insert(0,item);
-    }
-
-    // Non-generic method
-    public static List<int> ConverToIntList(this List<decimal> decimals)
-    {
-        var intsList = new List<int>();
-
-        foreach (var item in decimals)
-        {
-            intsList.Add((int)item);
-        }
-        return intsList;
-    }
-
-    // Converting from one type to another using T
-    public static List<TTarget> ConvertTo<TSource, TTarget>(
-        this List<TSource> sourceList)
-    {
-        var targetList = new List<TTarget>();
-
-        foreach (var item in sourceList)
-        {
-            var convertedItem =
-                (TTarget)Convert.ChangeType(item,typeof(TTarget));
-
-            targetList.Add(convertedItem);
-        }
-
-        return targetList;
-
+        this.x = x;
+        this.y = y;
     }
 }
