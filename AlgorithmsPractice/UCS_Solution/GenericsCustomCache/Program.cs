962281291f37f0312680685ea212ef7805f1beff
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-09 13:24:08 -0500

    improving list performance
    list size defined at start

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,19 +1,30 @@
-﻿
+using System.Diagnostics;
+
+// Getting how much it takes to execute the method
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+//var ints = CreateCollectionOfRandomLengthWithConstraints<DateTime>(0);
+//stopwatch.Stop();
+//Console.WriteLine($"Execution took {stopwatch.ElapsedMilliseconds} ms.");
+// It took ~1.94 sec.
+
+var ints2 = CreateCollectionWithFixedSize<DateTime>(0);
+stopwatch.Stop();
+Console.WriteLine($"Execution took {stopwatch.ElapsedMilliseconds} ms.");
+// It took ~1,49 sec.
+
 Console.ReadKey();
 
-// Method with 1 parameter
-void SomeMethod<TPet>(TPet pet) where TPet : Pet, IComparable<TPet>
+IEnumerable<T> CreateCollectionWithFixedSize<T>(
+    int maxLength) where T : new()
 {
+    var length = 100000000;
+    var resultList = new List<T>(length);
 
+    for (int i = 0; i < length; i++)
+    {
+        resultList.Add(new T());
+    }
+
+    return resultList;
 }
-
-// Method with 2 parameters
-void SomeMethod2<TPet, TOwner>(TPet pet,TOwner owner)
-    where TPet : Pet, IComparable<TPet>
-    where TOwner : new()
-{
-
-}
-
-public class Pet { }
-public class PetOwner { }
