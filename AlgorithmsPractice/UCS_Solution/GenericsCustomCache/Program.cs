dad765eb436b4d72f1699364855977a439a5aeba
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-09 16:55:55 -0500

    generics with numbers interface
    INumber<T>

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,46 +1,13 @@
-﻿
-var john = new Person { Name = "John",YearOfBirth = 1980 };
-var anna = new Person { Name = "Anna",YearOfBirth = 1915 };
+﻿using System.Numerics;
 
-PrintInOrder(10,5);
-PrintInOrderGeneric(10,5);
-PrintInOrderGeneric("aaa","bbb");
-PrintInOrderGeneric(anna,john);
+Console.WriteLine($"Sq of 2: {Calculator.Square(2)}");
+Console.WriteLine($"Sq of 3m: {Calculator.Square(3m)}");
+Console.WriteLine($"Sq of 4d: {Calculator.Square(4d)}");
 
 Console.ReadKey();
 
-void PrintInOrder(int first,int second)
+public static class Calculator
 {
-    if (first < second) Console.WriteLine($"{first}, {second}");
-    else if (first > second) Console.WriteLine($"{second}, {first}");
-}
-
-void PrintInOrderGeneric<T>(T first,T second)
-    where T : IComparable<T>
-{
-    // if called in the first object and returns a positive number (true),
-    // first should be moved after the second
-    if (first.CompareTo(second) > 0)
-    {
-        Console.WriteLine($"{second}, {first}");
-    }
-    else
-    {
-        Console.WriteLine($"{first}, {second}");
-    }
-}
-
-public class Person : IComparable<Person>
-{
-    public string Name { get; init; }
-    public int YearOfBirth { get; init; }
-
-    public int CompareTo(Person other)
-    {
-        if (YearOfBirth < other.YearOfBirth) return 1;
-        else if (YearOfBirth > other.YearOfBirth) return -1;
-        else return 0;
-    }
-
-    public override string ToString() => $"{Name} born in {YearOfBirth}";
+    public static T Square<T>(T input) where T : INumber<T>
+        => input * input;
 }
