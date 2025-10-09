926481ce4d5fd74cf7512e57bf01d3c089ac332f
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-09 13:50:45 -0500

    type constraints in methods returning a collection

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,30 +1,81 @@
-﻿using System.Diagnostics;
+﻿var people = new List<Person>
+{
+    new Person{Name = "Jhon", YearOfBirth = 1980},
+    new Person {Name = "Anna", YearOfBirth = 1815},
+    new Person{Name = "Bill", YearOfBirth = 2150}
+};
 
-// Getting how much it takes to execute the method
-Stopwatch stopwatch = Stopwatch.StartNew();
+var employees = new List<Employee>
+{
+    new Employee {Name = "Jhon", YearOfBirth = 1980},
+    new Employee {Name = "Anna", YearOfBirth = 1815},
+    new Employee {Name = "Bill", YearOfBirth = 2150}
+};
 
-//var ints = CreateCollectionOfRandomLengthWithConstraints<DateTime>(0);
-//stopwatch.Stop();
-//Console.WriteLine($"Execution took {stopwatch.ElapsedMilliseconds} ms.");
-// It took ~1.94 sec.
+var validPeople = GetOnlyValid(people);
+var validEmployees = GetOnlyValid(employees);
 
-var ints2 = CreateCollectionWithFixedSize<DateTime>(0);
-stopwatch.Stop();
-Console.WriteLine($"Execution took {stopwatch.ElapsedMilliseconds} ms.");
-// It took ~1,49 sec.
+foreach (var employee in validEmployees)
+{
+    // GoToWork is defined in Employee class, but the current
+    // result of GetOnlyValid is an IEnumerable<Person>
+    // Casting may work, but it is performance expensive
+    employee.GoToWork(); // compile error
+}
+
+var validPeople1 = GetOnlyValidWithGenerics(people);
+var validEmployees1 = GetOnlyValidWithGenerics(employees);
+
+foreach (var employee in validEmployees1)
+{
+    // validEmployees1 is a collection of Employee
+    // casting not required
+    employee.GoToWork();
+}
 
 Console.ReadKey();
 
-IEnumerable<T> CreateCollectionWithFixedSize<T>(
-    int maxLength) where T : new()
+IEnumerable<Person> GetOnlyValid(IEnumerable<Person> persons)
 {
-    var length = 100000000;
-    var resultList = new List<T>(length);
-
-    for (int i = 0; i < length; i++)
+    var result = new List<Person>();
+    foreach (var person in persons)
     {
-        resultList.Add(new T());
+        if (person.YearOfBirth > 1900 &&
+            person.YearOfBirth < DateTime.Now.Year)
+        {
+            result.Add(person);
+        }
     }
+    return result;
+}
 
-    return resultList;
+// TPerson can be of any type derived from Person
+// Use it when you want to operate on classes derived from Person
+// If not sure, use
+// IEnumerable<Person> GetOnlyValid(IEnumerable<TPerson> persons)
+// and only add constraints if you are certain you need them.
+IEnumerable<TPerson> GetOnlyValidWithGenerics<TPerson>(IEnumerable<TPerson> persons)
+    where TPerson : Person
+{
+    var result = new List<TPerson>();
+    foreach (var person in persons)
+    {
+        if (person.YearOfBirth > 1900 &&
+            person.YearOfBirth < DateTime.Now.Year)
+        {
+            result.Add(person);
+        }
+    }
+    return result;
+}
+
+public class Person
+{
+    public string Name { get; init; }
+    public int YearOfBirth { get; init; }
+}
+
+public class Employee : Person
+{
+    public void GoToWork() => Console.WriteLine("Going to work");
 }
