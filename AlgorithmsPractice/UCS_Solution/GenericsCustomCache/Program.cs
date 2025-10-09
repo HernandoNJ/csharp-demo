e2b4a113e8d83d9df0be38ba0fc05f627efb930b
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-09 16:50:14 -0500

    print in order only for IComparable types

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,58 +1,46 @@
-﻿var numbers = new List<int> { 5,1,7,2 };
-numbers.Sort();
+﻿
+var john = new Person { Name = "John",YearOfBirth = 1980 };
+var anna = new Person { Name = "Anna",YearOfBirth = 1915 };
 
-var words = new List<string> { "ddd","aaa","ccc","bbb" };
-words.Sort();
-
-var people = new List<Person>
-{
-    new Person{Name = "Jhon", YearOfBirth = 1980},
-    new Person {Name = "Anna", YearOfBirth = 1815},
-    new Person{Name = "Bill", YearOfBirth = 2150}
-};
-
-// Invalid operation exception: The Person list doesn't implement
-// IComparable to call someObject.CompareTo(otherObject)
-// C# doesn't know how to order items from People
-// people.Sort();
-
-var people1 = new List<ComparablePerson>
-{
-    new ComparablePerson{Name = "Jhon", YearOfBirth = 1980},
-    new ComparablePerson{Name = "Anna", YearOfBirth = 1815},
-    new ComparablePerson{Name = "Bill", YearOfBirth = 2150}
-};
-
-people1.Sort();
+PrintInOrder(10,5);
+PrintInOrderGeneric(10,5);
+PrintInOrderGeneric("aaa","bbb");
+PrintInOrderGeneric(anna,john);
 
 Console.ReadKey();
 
-public class Person
+void PrintInOrder(int first,int second)
 {
-    public string Name { get; init; }
-    public int YearOfBirth { get; init; }
+    if (first < second) Console.WriteLine($"{first}, {second}");
+    else if (first > second) Console.WriteLine($"{second}, {first}");
 }
 
-// ComparablePerson : IComparable<Person>
-// used to compare 2 objects of the same type
-public class ComparablePerson : IComparable<ComparablePerson>
+void PrintInOrderGeneric<T>(T first,T second)
+    where T : IComparable<T>
 {
-    public string Name { get; init; }
-    public int YearOfBirth { get; init; }
-
-    // Younger to the left, older to the right
-    public int CompareTo(ComparablePerson other)
+    // if called in the first object and returns a positive number (true),
+    // first should be moved after the second
+    if (first.CompareTo(second) > 0)
     {
-        if (YearOfBirth == other.YearOfBirth) return 0;
-        return YearOfBirth < other.YearOfBirth ? 1 : -1;
+        Console.WriteLine($"{second}, {first}");
+    }
+    else
+    {
+        Console.WriteLine($"{first}, {second}");
     }
 }
 
-// if a < b is true return 1,   b goes left => b ... a
-// if a < b is false return -1, a goes left => a ... b
+public class Person : IComparable<Person>
+{
+    public string Name { get; init; }
+    public int YearOfBirth { get; init; }
 
-// 1980 1815 2150
-// 1980 < 1815 F    1980 younger left ... 1815 older right
-// 2150 < 1980 F    2150 younger left ... 1980 older right 
-// 2150 < 1815 F    2150 younger left ... 1815 older right
-// *** 2150 1980 1815
+    public int CompareTo(Person other)
+    {
+        if (YearOfBirth < other.YearOfBirth) return 1;
+        else if (YearOfBirth > other.YearOfBirth) return -1;
+        else return 0;
+    }
+
+    public override string ToString() => $"{Name} born in {YearOfBirth}";
+}
