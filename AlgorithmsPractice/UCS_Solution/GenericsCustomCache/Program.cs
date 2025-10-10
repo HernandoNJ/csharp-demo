0790395d05c6468f4acc15ac7717f999050c4640
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-10 12:58:04 -0500

    dictionaries 1

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,20 +1,33 @@
-var numbers = new[] { 1,4,7,19,2 };
+var countryToCurrencyMapping = new Dictionary<string,string>();
 
-Console.WriteLine("Is any number greater than 10, "
-                  + IsAny(numbers,(n) => n > 10));
+// Modern way to initialize a dictionary
+var countryToCurrencyMapping1 = new Dictionary<string,string>
+{
+    ["USA"] = "USD",
+    ["IND"] = "INR",
+    ["SPA"] = "EUR"
+};
 
-Console.WriteLine("Is any number even,"
-                  + IsAny(numbers,(n) => n % 2 == 0));
+// Old way to initialize a dictionary
+var countryToCurrencyMapping2 = new Dictionary<string,string>
+{
+    { "USA", "USD" },
+    { "IND", "INR" },
+    { "SPA", "EUR" }
+};
+
+countryToCurrencyMapping.Add("USA","USD");
+countryToCurrencyMapping.Add("IND","INR");
+countryToCurrencyMapping.Add("SPA","EUR");
+countryToCurrencyMapping.Add("ITA","EUR");
+countryToCurrencyMapping.Add("FRA","EUR");
+
+Console.WriteLine($"SPA currency is: {countryToCurrencyMapping["SPA"]}");
+
+countryToCurrencyMapping["PND"] = "PLN";
+foreach (var country in countryToCurrencyMapping)
+{
+    Console.WriteLine(country.Key + ", " + country.Value);
+}
 
 Console.ReadKey();
-
-bool IsAny(IEnumerable<int> numbers,
-           Func<int,bool> isValidPredicate)
-{
-    foreach (int number in numbers)
-    {
-        if (isValidPredicate(number)) return true;
-    }
-
-    return false;
-}
