a6f73d8549d5ca50794e013147239f9ca2e69da5
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-10 18:07:50 -0500

    new version of strategy design pattern
    SRP broke

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -21,54 +21,41 @@ void Print(IEnumerable<int> numbers)
 
 public class NumbersFilter
 {
+    // Currently the class has 2 functions, breaking SRP
+    // Translate the user input to a way of filtering
+    // Filter a collection with Select()
+
+    Func<int, bool> isEven = (n) => n % 2 == 0;
+    Func<int, bool> isOdd = (n) => n % 2 != 0;
+    Func<int, bool> isPositive = (n) => n > 0;
+
     public List<int> FilterBy(string filterType,
                               List<int> numbers)
     {
         switch(filterType)
         {
             case "Even":
-                return SelectEvenNumbers(numbers);
+                //return Select(numbers, number => number % 2 == 0);
+                return Select(numbers, isEven);
             case "Odd":
-                return SelectOddNumbers(numbers);
+                //return Select(numbers, number => number % 2 == 1);
+                return Select(numbers, isOdd);
             case "Positive":
-                return SelectPositiveNumbers(numbers);
-            default: throw new NotSupportedException($"{filterType} is not a valid filter");
+                //return Select(numbers, number => number > 0);
+                return Select(numbers, isPositive);
+            default:
+                throw new NotSupportedException($"{filterType} is not a valid filter");
         }
     }
 
-    private List<int> SelectEvenNumbers(List<int> numbers)
+    public List<int> Select(List<int> numbers,
+                            Func<int, bool> isValid)
     {
         var result = new List<int>();
 
         foreach(var number in numbers)
         {
-            if(number % 2 == 0)
-                result.Add(number);
-        }
-
-        return result;
-    }
-
-    private List<int> SelectOddNumbers(List<int> numbers)
-    {
-        var result = new List<int>();
-
-        foreach(var number in numbers)
-        {
-            if(number % 2 != 0)
-                result.Add(number);
-        }
-
-        return result;
-    }
-
-    private List<int> SelectPositiveNumbers(List<int> numbers)
-    {
-        var result = new List<int>();
-
-        foreach(var number in numbers)
-        {
-            if(number > 0)
+            if(isValid(number))
                 result.Add(number);
         }
 
