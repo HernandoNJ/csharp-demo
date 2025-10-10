e2bf186a4ad519a0492598a9fb800da836260075
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-10 17:31:11 -0500

    initial example, strategy design pattern
    code duplication
    issues for scaling

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,63 +1,73 @@
-var employees = new List<Employee>
+var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+
+Console.WriteLine($@"Select filter:
+Odd
+Even
+");
+
+var userInput = Console.ReadLine();
+
+List<int> result;
+
+switch(userInput)
 {
-    new Employee("Jake", "Space nav", 25000),
-    new Employee("Anna", "Space nav", 29000),
-    new Employee("Barbara", "Xenobio", 21500),
-    new Employee("Damien", "Xenobio", 22000),
-    new Employee("Nisha", "Mechanics", 21000),
-    new Employee("Gustavo", "Mechanics", 20000)
-};
+    case "Even":
+        result = SelectEvenNumbers(numbers);
+        break;
+    case "Odd":
+        result = SelectOddNumbers(numbers);
+        break;
+    case "Positive":
+        result = SelectPositiveNumbers(numbers);
+        break;
+    default:
+        throw new NotSupportedException($"{userInput} is not a valid filter");
+}
 
-var departmentsAverageSalary = CalculateAverageSalaryPerDepartment(employees);
-
-foreach(var item in departmentsAverageSalary)
-    Console.WriteLine($"Department: {item.Key}, "
-                      + $"Average salary: {item.Value}");
+Print(result);
 
 Console.ReadKey();
 
-Dictionary<string, decimal> CalculateAverageSalaryPerDepartment(IEnumerable<Employee> employees)
+void Print(IEnumerable<int> numbers)
 {
-    var departments = new Dictionary<string, List<Employee>>();
+    Console.WriteLine(string.Join(",", numbers));
+}
 
-    // Grouping department and employees List
-    foreach(var employee in employees)
+List<int> SelectEvenNumbers(List<int> numbers)
+{
+    var result = new List<int>();
+
+    foreach(var number in numbers)
     {
-        if(!departments.ContainsKey(employee.Department))
-            departments[employee.Department] = new List<Employee>();
-
-        departments[employee.Department].Add(employee);
-    }
-
-    var result = new Dictionary<string, decimal>();
-
-    // department key: department's name
-    // department value: List<Employee>
-    foreach(var department in departments)
-    {
-        var salariesSum = 0m;
-
-        // loop over each employee in department.Value ... List<Employee>
-        foreach(var employee in department.Value)
-            salariesSum += employee.MonthlySalary;
-
-        var average = salariesSum / department.Value.Count;
-        result[department.Key] = average;
+        if(number % 2 == 0)
+            result.Add(number);
     }
 
     return result;
 }
 
-public class Employee
+List<int> SelectOddNumbers(List<int> numbers)
 {
-    public Employee(string name, string department, decimal monthlySalary)
+    var result = new List<int>();
+
+    foreach(var number in numbers)
     {
-        Name = name;
-        Department = department;
-        MonthlySalary = monthlySalary;
+        if(number % 2 != 0)
+            result.Add(number);
     }
 
-    public string Name { get; init; }
-    public string Department { get; init; }
-    public decimal MonthlySalary { get; init; }
+    return result;
+}
+
+List<int> SelectPositiveNumbers(List<int> numbers)
+{
+    var result = new List<int>();
+
+    foreach(var number in numbers)
+    {
+        if(number > 0)
+            result.Add(number);
+    }
+
+    return result;
 }
