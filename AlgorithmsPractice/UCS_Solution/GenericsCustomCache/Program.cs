25a005259d3e887cb806fe9e1e3eec518dd71839
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-10 23:22:18 -0500

    cache project, item with id added to dictionary

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,72 +1,74 @@
-var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
-var filteringStrategiesSelector = new FilteringStrategySelector();
+var dataDownloader = new SlowDataDownloader();
 
-Console.WriteLine("Select filter");
-Console.WriteLine(
-    string.Join(
-        Environment.NewLine,
-        filteringStrategiesSelector
-        .FilteringStrategiesNames));
-
-var userInput = Console.ReadLine();
-
-var filteringStrategy =
-    new FilteringStrategySelector()
-    .Select(userInput);
-
-var result =
-    new Filter()
-    .FilterBy(numbers, filteringStrategy);
-
-Print(result);
-
-var words = new[] { "zebra", "ostric", "orch" };
-var owords =
-    new Filter()
-    .FilterBy(
-        words,
-        word => word.StartsWith("o"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
-void Print(IEnumerable<int> numbers)
+
+// after the data identified by ID “id1” is fetched for the first time, the next time, it could be served from the cache. 
+
+// A generic Cache class is defined and used. It should be able to work for any type of key and data, not only strings like here.
+// The first time we fetch the data identified by a certain key, it still works slowly. But the next time this data is fetched it should be served from the cache, so it should happen almost immediately. 
+
+public interface IDataDownloader
 {
-    Console.WriteLine(string.Join(",", numbers));
+    string DownloadData(string resourceId);
 }
 
-public class Filter
+public class SlowDataDownloader : IDataDownloader
 {
-    // Code reusability because it works with any data type (T)
-    // eliminating code duplication
-    // Type safety by enforcing correct usage at compile time
-    // Flexibility by allowing IEnumerable<T>, not just lists
-    public IEnumerable<T> FilterBy<T>(IEnumerable<T> items,
-                                      Func<T, bool> predicate)
+    public CacheManager<string, string> cacheManager = new CacheManager<string, string>();
+
+    public string DownloadData(string resourceId)
     {
-        var result = new List<T>();
+        // let's imagine this method downloads real data very slowly
+        Thread.Sleep(1000);
 
-        foreach(var item in items)
-        {
-            if(predicate(item))
-                result.Add(item);
-        }
+        var resourceData = "Data package loaded for " + resourceId;
 
-        return result;
+        if(!cacheManager.HasKey(resourceId))
+            cacheManager.AddItem(resourceId, resourceData);
+        else
+            Console.WriteLine($"Item already cached. id: {resourceId}, data: {cacheManager.GetItem(resourceId)}");
+
+        return resourceId + " data downloaded";
     }
 }
 
-public class FilteringStrategySelector
+public class FastDataDownloader : IDataDownloader
 {
-    private readonly Dictionary<string, Func<int, bool>> filteringStrategies = new()
+    public string DownloadData(string resourceId)
     {
-        ["Even"] = number => number % 2 == 0,
-        ["Odd"] = number => number % 2 == 1,
-        ["Positive"] = number => number > 0,
-        ["Negative"] = number => number < 0
-    };
+        // Check if the resourceId is in the cache dictionary
 
-    public Func<int, bool> Select(string filteringType)
-        => filteringStrategies[filteringType];
+        // let's imagine this method downloads real data very slowly
+        //Thread.Sleep(1000);
+        //var resourceData = "Some data for " + resourceId;
+        //var cache = new StringCache(resourceId, resourceData);
 
-    public IEnumerable<string> FilteringStrategiesNames => filteringStrategies.Keys;
+        return "Some data for " + resourceId;
+    }
+}
+
+public class CacheManager<T1, T2>
+{
+    private readonly Dictionary<T1, T2> _cacheDictionary = new Dictionary<T1, T2>();
+
+    public bool HasKey(T1 key)
+        => _cacheDictionary.ContainsKey(key);
+
+    public void AddItem(T1 id, T2 data)
+    {
+        if(!_cacheDictionary.ContainsKey(id))
+            _cacheDictionary.Add(id, data);
+    }
+
+    public T2 GetItem(T1 id)
+        => _cacheDictionary[id];
 }
