e72e2b53cf5315eed35dd608552a1b943527f7e2
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-11 16:10:28 -0500

    decorator design pattern 1

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,16 +1,47 @@
-var slowDataDownloader = new SlowDataDownloader();
+// Decorator pattern applied
+// Here, it can be chosen the way of downloading
+// SlowDataDownloader or CachingDataDownloader
+
+//var dataDownloader = new SlowDataDownloader();
+// CachingDataDownloader works as a wrapper around SlowDataDownloader
+// enriching it with a caching feature
+var dataDownloader =
+    new CachingDataDownloader(new SlowDataDownloader());
 
 var idsList = new List<string>
 { "id1", "id2", "id3", "id1", "id3", "id1", "id2"};
 
-DownloadData(idsList);
+foreach(var id in idsList)
+{
+    Console.WriteLine(dataDownloader.DownloadData(id));
+}
 
 Console.ReadKey();
 
-void DownloadData(List<string> ids)
+// OCP applied
+// To apply the decorator pattern in this class
+// It need to implement the same interface as the decorated type
+// with a new version
+// Now, this class is in charge of caching, not about how the data is accessed
+public class CachingDataDownloader : IDataDownloader
 {
-    foreach(var id in ids)
-        Console.WriteLine(slowDataDownloader.DownloadData(id));
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    public CachingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    // As we are using the decorator pattern on SlowDataDownloader
+    // This class will implement IDataDownloader,
+    //  Assign a SlowDataDownloader instance when using the constructor, 
+    //  Add cache functionality
+    //  and call SlowDataDownloader.DownloadData
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
 }
 
 public interface IDataDownloader
@@ -18,19 +49,13 @@ public interface IDataDownloader
     string DownloadData(string resourceId);
 }
 
+// This class is fully dedicated to download data and not about caching it
 public class SlowDataDownloader : IDataDownloader
 {
-    private readonly Cache<string, string> _cache = new();
-
     public string DownloadData(string resourceId)
     {
-        return _cache.Get(resourceId, DownloadDataWithoutCaching);
-    }
-
-    private string DownloadDataWithoutCaching(string resourceId)
-    {
         Thread.Sleep(1000);
-        return resourceId + " package data";
+        return $"Some data for {resourceId}";
     }
 }
 
@@ -41,9 +66,7 @@ public class Cache<TKey, TData>
     public TData Get(TKey key, Func<TKey, TData> getDataForTheFirstTime)
     {
         if(!_cachedData.ContainsKey(key))
-        {
             _cachedData[key] = getDataForTheFirstTime(key);
-        }
 
         return _cachedData[key];
     }
