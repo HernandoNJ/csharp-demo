faca330a22ebcd140ed82762b03045ebe034fa6a
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-10 21:36:44 -0500

    strategy pattern example improved
    refactored, generic implementation

--- AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
+++ AlgorithmsPractice/UCS_Solution/GenericsCustomCache/Program.cs
@@ -1,10 +1,12 @@
 var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+var filteringStrategiesSelector = new FilteringStrategySelector();
 
-Console.WriteLine($@"Select filter:
-Odd
-Even
-Positive
-");
+Console.WriteLine("Select filter");
+Console.WriteLine(
+    string.Join(
+        Environment.NewLine,
+        filteringStrategiesSelector
+        .FilteringStrategiesNames));
 
 var userInput = Console.ReadLine();
 
@@ -12,11 +14,18 @@ var filteringStrategy =
     new FilteringStrategySelector()
     .Select(userInput);
 
-List<int> result =
-    new NumbersFilter()
+var result =
+    new Filter()
     .FilterBy(numbers, filteringStrategy);
 
 Print(result);
+
+var words = new[] { "zebra", "ostric", "orch" };
+var owords =
+    new Filter()
+    .FilterBy(
+        words,
+        word => word.StartsWith("o"));
 
 Console.ReadKey();
 
@@ -25,17 +34,21 @@ void Print(IEnumerable<int> numbers)
     Console.WriteLine(string.Join(",", numbers));
 }
 
-public class NumbersFilter
+public class Filter
 {
-    public List<int> FilterBy(List<int> numbers,
-                              Func<int, bool> predicate)
+    // Code reusability because it works with any data type (T)
+    // eliminating code duplication
+    // Type safety by enforcing correct usage at compile time
+    // Flexibility by allowing IEnumerable<T>, not just lists
+    public IEnumerable<T> FilterBy<T>(IEnumerable<T> items,
+                                      Func<T, bool> predicate)
     {
-        var result = new List<int>();
+        var result = new List<T>();
 
-        foreach(var number in numbers)
+        foreach(var item in items)
         {
-            if(predicate(number))
-                result.Add(number);
+            if(predicate(item))
+                result.Add(item);
         }
 
         return result;
@@ -48,9 +61,12 @@ public class FilteringStrategySelector
     {
         ["Even"] = number => number % 2 == 0,
         ["Odd"] = number => number % 2 == 1,
-        ["Positive"] = number => number > 0
+        ["Positive"] = number => number > 0,
+        ["Negative"] = number => number < 0
     };
 
     public Func<int, bool> Select(string filteringType)
         => filteringStrategies[filteringType];
+
+    public IEnumerable<string> FilteringStrategiesNames => filteringStrategies.Keys;
 }
