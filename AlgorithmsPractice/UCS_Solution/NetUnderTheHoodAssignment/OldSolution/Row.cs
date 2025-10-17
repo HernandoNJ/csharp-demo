4503c83c3b4992ef4a9d6b57fc33d407d40769d9
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-17 12:09:07 -0500

    code reset to work on csv improvements

--- AlgorithmsPractice/UCS_Solution/NetUnderTheHoodAssignment/OldSolution/Row.cs
+++ AlgorithmsPractice/UCS_Solution/NetUnderTheHoodAssignment/OldSolution/Row.cs
@@ -3,30 +3,14 @@
 public class Row
 {
     private Dictionary<string, object> _data;
-    private readonly Dictionary<string, string> StringData = [];
-    private readonly Dictionary<string, int> IntData = [];
-    private readonly Dictionary<string, float> FloatData = [];
-    private readonly Dictionary<string, bool> BoolData = [];
 
-    public Row(Dictionary<string, object> data,
-               Dictionary<string, string> stringData,
-               Dictionary<string, int> intData,
-               Dictionary<string, float> floatData,
-               Dictionary<string, bool> boolData)
+    public Row(Dictionary<string, object> data)
     {
-        StringData = stringData;
-        IntData = intData;
-        FloatData = floatData;
-        BoolData = boolData;
         _data = data;
     }
 
     public object GetAtColumn(string columnName)
     {
-        if(_data.ContainsKey(columnName))
-        {
-            return _data[columnName];
-        }
-        return null;
+        return _data[columnName];
     }
 }
