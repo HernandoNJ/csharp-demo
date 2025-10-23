aebcac567fbfcfe55e6cac1cf0b5681a22fd6d3e
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-23 15:51:20 -0500

    reflection basic example

--- UCS_Solution/AdvancedTypes/Program.cs
+++ UCS_Solution/AdvancedTypes/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Using reflection to access the information about some type 
+// at runtime and read the names and values of its properties
+var converter = new ObjectToTextConverter();
+Console.WriteLine(
+    converter.Convert(
+        new House("123 Maple Road", 170.6f, 2)));
+
+Console.ReadKey();
+
+public class ObjectToTextConverter
+{
+    public string Convert(object obj)
+    {
+        Type type = obj.GetType();
+        var properties = type
+            .GetProperties()
+            .Where(p => p.Name != "EqualityContract");
+
+        return string.Join(
+            ", ",
+            properties
+            .Select(property =>
+            $"{property.Name} is {property.GetValue(obj)}"));
+    }
+
+}
+
+public record Pet(string Name, PetType PetType, float Weight);
+public record House(string Address, float Area, int Floors);
+public enum PetType { Cat, Dog, Fish }
