49a9150c3d0b37e9abb7e36cc7d061fdc30f4bc3
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-29 11:51:37 -0500

    with expression - non-destructive mutation

--- UCS_Solution/AdvancedTypes/Program.cs
+++ UCS_Solution/AdvancedTypes/Program.cs
@@ -1,26 +1,22 @@
-﻿var point = new Point(1, 3);
-var anotherPoint = point;
-anotherPoint.Y = 100;
+﻿// moving point.x 1 unit to the right directly is not possible ... point.X += 1;
+var point = new Point(10, 20);
 
-Point a = null;
+// using with to create a new object with the modified value of point.x
+var movedPoint1 = point with { X = 11 };
+var movedPoint2 = point with { X = point.X +1 };
 
-Console.WriteLine("point is: " + point);
-Console.WriteLine("another point is: " + anotherPoint);
-
-SomeMethod(5);
-SomeMethod(new Person());
+// Conditions for using with: the Properties (X,Y)
+// Must be public, have get and init or set 
+// It only works for structs, records and record structs and anonymous types
+var pet = new { Name = "Lara", Type = "Fish" };
+var pet2 = pet with { Type = "Bird" };
 
 Console.ReadKey();
 
-void SomeMethod<T>(T param) where T : struct
-{
-
-}
-
 struct Point
 {
-    public int X { get; set; }
-    public int Y { get; set; }
+    public int X { get; init; }
+    public int Y { get; init; }
 
     public Point(int x, int y)
     {
