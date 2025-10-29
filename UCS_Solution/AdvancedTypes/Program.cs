8898f3df0eb7e598c8009415481b15cb7cc1eeca
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-29 12:03:05 -0500

    enforcing immutability in structs

--- UCS_Solution/AdvancedTypes/Program.cs
+++ UCS_Solution/AdvancedTypes/Program.cs
@@ -1,20 +1,9 @@
-﻿// moving point.x 1 unit to the right directly is not possible ... point.X += 1;
-var point = new Point(10, 20);
-
-// using with to create a new object with the modified value of point.x
-var movedPoint1 = point with { X = 11 };
-var movedPoint2 = point with { X = point.X +1 };
-
-// Conditions for using with: the Properties (X,Y)
-// Must be public, have get and init or set 
-// It only works for structs, records and record structs and anonymous types
-var pet = new { Name = "Lara", Type = "Fish" };
-var pet2 = pet with { Type = "Bird" };
-
+﻿
 Console.ReadKey();
 
-struct Point
+readonly struct Point
 {
+    private readonly int z;
     public int X { get; init; }
     public int Y { get; init; }
 
