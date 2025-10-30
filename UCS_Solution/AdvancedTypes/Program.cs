86cfbb4ac519ccf7b4b3623fbca4c0b6d300980d
HernandoNJ <hernandonj.unity@gmail.com>
2025-10-30 18:47:20 -0500

    code cleanup

--- UCS_Solution/AdvancedTypes/Program.cs
+++ UCS_Solution/AdvancedTypes/Program.cs
@@ -1,18 +1,15 @@
-﻿var john = new Person(1, "John");
+﻿var john1 = new Person(1, "John");
 var marie1 = new Person(1, "Marie");
 var marie2 = new Person(2, "Marie");
 
-Console.WriteLine("john.Equals(marie1): " + john.Equals(marie1));
-Console.WriteLine("john.Equals(marie2): " + john.Equals(marie2));
+Console.WriteLine("john.Equals(marie1): " + john1.Equals(marie1));
+Console.WriteLine("john.Equals(marie2): " + john1.Equals(marie2));
 
 var point1 = new Point(1, 5);
 var point2 = new Point(1, 5);
-var areEqual = point1.Equals(point2);
+Console.WriteLine("point1.Equals(point2): " + point1.Equals(point2));
 
 Console.ReadKey();
-
-// IEquatable<T> compares if 2 objects are equal and returns bool
-// IComparable<T> checks in what order objects should appear if sorted and return an int (-1,0,1)
 
 struct Point : IEquatable<Point>
 {
@@ -25,29 +22,14 @@ struct Point : IEquatable<Point>
         Y = y;
     }
 
-    public override string ToString() => $"X: {X}, Y: {Y}";
+    public override string ToString()
+        => $"X: {X}, Y: {Y}";
 
-    // Equals takes obj as parameter, so an argument of type Point (struct) must be boxed
-    // Check if obj is of type Point. If so, obj is unboxed
-    // Fields and properties are compared using reflection
-    // Boxing, Unboxing, Reflection
-    // Even if avoiding reflection, boxing and unboxing is applied
-    // It is defined in the base class object as public virtual bool Equals(object? obj);
     public override bool Equals(object? obj)
-    {
-        return obj is Point point &&
-               Equals(point);
-    }
+        => obj is Point point && Equals(point);
 
-    // Avoiding Boxing, Unboxing, because other is of type Point (struct), not object
-    // If the runtime sees 2 methods with the same name in a type, it will choose the more specialized one
-    // This is the method that implements IEquatable ... bool Equals(T? other);
-    // T can be any type
     public bool Equals(Point other)
-    {
-        return X == other.X &&
-               Y == other.Y;
-    }
+        => X == other.X && Y == other.Y;
 }
 
 public class Person
@@ -61,11 +43,6 @@ public class Person
         Name = name;
     }
 
-    // Overriding Equals() in a reference type.
-    // It's recommended to also override GetHashCode() when overriding Equals.
     public override bool Equals(object? obj)
-    {
-        return obj is Person other &&
-               Id == other.Id;
-    }
+        => obj is Person other && Id == other.Id;
 }
