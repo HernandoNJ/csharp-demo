10f592dad4d7bfaec7f98ba6f746551db6f5b474
HernandoNJ <hernandonj.unity@gmail.com>
2025-11-12 22:18:39 -0500

    added StartWarsPlanetsStatsApp class
    added global try/catch block in program.
    moved code from Program to StartWarsPlanetsStatsApp class, applied DI and added main and secondary api data readers.

--- UCS_Solution/StarWarsPlanetsStats/Program.cs
+++ UCS_Solution/StarWarsPlanetsStats/Program.cs
@@ -2,38 +2,58 @@
 using StarWarsPlanetsStats.ApiDataAccess;
 using StarWarsPlanetsStats.DTOs;
 
-// Documentation comments
-// Possible exceptions
-// **********************************************************
-// What happens if an exception is thrown? the program should continue? Or print error message and shut down?
-// For ApiDataReader, if the http request fails, mock api can be used
-
-var baseAddress = "https://swapi.dev/";
-var requestUri = "api/planets";
-string? json = null;
-
-// Adding try/catch block for api data reader
+// This global try/catch block will intercept all exceptions thrown in the program that were not caught elsewhere.
 try
 {
-    // API may be down and/or expected data not provided
-    IApiDataReader apiDataReader = new ApiDataReader();
-    json = await apiDataReader.Read(baseAddress, requestUri);
+    await new StartWarsPlanetsStatsApp(
+        new ApiDataReader(), 
+        new MockStarWarsApiDataReader()).RunAsync();
 }
-catch (HttpRequestException ex)
+catch (Exception ex)
 {
-    Console.WriteLine("API request unsuccessful, switching to mock api data. Exception message: " + ex.Message);
+    Console.WriteLine("An error occurred. Exception message: " + ex.Message);
 }
-
-if(json is null)
-{
-    IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
-    json = await apiDataReader.Read(baseAddress, requestUri);
-}
-
-// Deserialize<T>() contains 3 types of exceptions
-// Here the program execution may fail because the Json data may not be deserialized correctly
-// In this case, the program cannot continue and the developers must investigate the issue.
-var root = JsonSerializer.Deserialize<Root>(json);
 
 Console.WriteLine("Press any key to exit.");
 Console.ReadKey();
+
+public class StartWarsPlanetsStatsApp
+{
+    // Applying Dependency Inversion principle
+    // This class must not depend on concrete types but on abstractions
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
+
+    private readonly string baseAddress = "https://swapi.dev/";
+    private readonly string requestUri = "api/planets";
+
+    public StartWarsPlanetsStatsApp(IApiDataReader apiDataReader,
+                                    IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
+
+    public async Task RunAsync()
+    {
+        string? json = null;
+
+        // Adding try/catch block for api data reader
+        try
+        {
+            json = await _apiDataReader.Read(baseAddress, requestUri);
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("API request unsuccessful, switching to mock api data. Exception message: " + ex.Message);
+        }
+
+        if (json is null)
+        {
+            json = await _secondaryApiDataReader.Read(baseAddress, requestUri);
+        }
+
+        // If deserialization fails, the global try catch block throws an exception
+        var root = JsonSerializer.Deserialize<Root>(json);
+    }
+}
